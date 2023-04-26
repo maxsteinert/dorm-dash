2ad53dde5d2b2a1b4b8e5f7dbf9b4da84bec2869
Max <mks3@princeton.edu>
2023-04-26 14:09:03 -0400

    commit php file

--- process-order-form.php
+++ process-order-form.php
@@ -13,4 +13,28 @@ if (! $room) {
 }
 
 
-$host = "hostinger";
+$host = "localhost";
+$dbname = "u507831009_order_db";
+$username = "u507831009_maxsteinert";
+$password = "Ves6zJ]Pw/8";
+
+$conn = mysqli_connect($host, $username, $password, $dbname);
+
+if (mysqli_connect_errno()) {
+    die("Connection Error: " . mysqli_connect_error());
+}
+
+$sql = "INSERT INTO order_form (name, dorm, store)
+        VALUES ('$name', '$room', '$store')";
+
+$stmt = mysqli_stmt_init($conn);
+
+if (! mysqli_stmt_prepare($stmt, $sql)) {
+    die(mysqli_error($conn));
+}
+
+mysqli_stmt_bind_param($stmt, "sss", $name, $room, $store);
+
+mysqli_execute($stmt);
+
+echo "Record saved.";
